603e427bb95efac6c5324aef22069ffddf516543
Michael Tigue <mtigue@web4uonline.com>
2013-08-11 20:59:56 -0400

    1

---
+++ _vti_cnf/inc-header.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|26 Jul 2013 03:02:51 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_author:SR|web4u\\mtigue
+vti_modifiedby:SR|web4u\\mtigue
+vti_timecreated:TR|26 Jul 2013 03:02:51 -0000
+vti_cacheddtm:TX|26 Jul 2013 03:02:51 -0000
+vti_filesize:IR|460
+vti_backlinkinfo:VX|25/main.html
---
+++ inc-header.js
@@ -0,0 +1,6 @@
+﻿document.write("<div data-role='header' class='page-header' data-position='fixed' >");
+document.write("<a href='index.html' data-transition='none' data-icon='arrow-l' data-iconpos='notext' data-direction='reverse' data-rel='back'>Back</a>");
+document.write("<div class='title'>St. John's</div>");
+document.write("<a href='#home' data-transition='none' data-icon='home' data-iconpos='notext' class='ui-btn-right'>Home</a>");
+document.write("</div>");
+
