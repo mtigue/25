d5016fe1bc4410c1f636ef192bd44c116521bc63
Michael Tigue <mtigue@web4uonline.com>
2013-08-12 21:28:48 -0400

    5

---
+++ _vti_cnf/inc-footer.js
@@ -0,0 +1,12 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|12 Aug 2013 19:25:11 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_author:SR|web4u\\mtigue
+vti_modifiedby:SR|web4u\\mtigue
+vti_timecreated:TR|11 Aug 2013 10:54:18 -0000
+vti_syncwith_68.178.246.6\:21/yalecc:TX|12 Aug 2013 19:07:07 -0000
+vti_syncofs_68.178.246.6\:21/yalecc:TW|12 Aug 2013 19:28:01 -0000
+vti_nexttolasttimemodified:TW|12 Aug 2013 19:07:07 -0000
+vti_backlinkinfo:VX|
+vti_cacheddtm:TX|12 Aug 2013 19:07:07 -0000
+vti_filesize:IR|954
--- _vti_cnf/inc-header.js
+++ _vti_cnf/inc-header.js
@@ -1,9 +1,12 @@
 vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|26 Jul 2013 03:02:51 -0000
+vti_timelastmodified:TR|12 Aug 2013 18:42:12 -0000
 vti_extenderversion:SR|12.0.0.0
 vti_author:SR|web4u\\mtigue
 vti_modifiedby:SR|web4u\\mtigue
 vti_timecreated:TR|26 Jul 2013 03:02:51 -0000
-vti_cacheddtm:TX|26 Jul 2013 03:02:51 -0000
-vti_filesize:IR|460
 vti_backlinkinfo:VX|25/main.html
+vti_syncwith_68.178.246.6\:21/yalecc:TX|12 Aug 2013 18:42:12 -0000
+vti_syncofs_68.178.246.6\:21/yalecc:TW|12 Aug 2013 18:42:43 -0000
+vti_nexttolasttimemodified:TW|12 Aug 2013 15:30:41 -0000
+vti_cacheddtm:TX|12 Aug 2013 18:42:12 -0000
+vti_filesize:IR|565
